ab61a02ae14f12fa85d89eaf7c64d78773a44cd5
PC <daudraja185@gmail.com>
2026-08-13 12:44:34 +0500

    Clean up obsolete one-time deployment and debug scripts from backend root

--- check_deploy_status.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-c = paramiko.SSHClient()
-c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c.connect('141.136.44.191', username='root', password='.+H@/Dz5jYxtzs,+')
-
-stdin, stdout, stderr = c.exec_command('docker compose ps')
-print("DOCKER COMPOSE PS:\n", stdout.read().decode())
-c.close()
